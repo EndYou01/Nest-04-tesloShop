abc843d466782cf15fe30ed5aa3a4b6cdd7ca801
Jorge Senjudo <95231207+EndYou01@users.noreply.github.com>
2025-02-25 22:35:30 -0500

    Moving images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
-
+ 
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -17,8 +19,13 @@ import { FilesModule } from './files/files.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
-      synchronize: true
+      synchronize: true,
     }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
     ProductsModule,
     CommonModule,
     SeedModule,
